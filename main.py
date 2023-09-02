098f92e5a8f738d75d82c03492b9febac75b4cfa
Manillin <chri.vwm@gmail.com>
2023-09-02 17:10:29 +0200

    Latest fix

--- main.py
+++ main.py
@@ -28,12 +28,25 @@ lista_materie=[]
 lista_materie2=[]
 materia: Materia
 
+materia_holder = MatHolder()
+materia_holder.lista_materie = []
+
+
 materia = Materia("Sistemi Operativi",260)
-lista_materie.append(materia)
-for materia in lista_materie:
+#lista_materie.append(materia)
+
+print("\n\nPROVA MATERIA CLASS: ")
+materia_holder.lista_materie.append(materia)
+for materia in materia_holder.lista_materie:
     print(materia.get_n_materia())
+    print("Tempo totale in minuti: ",materia.get_time())
+    print(materia.get_time_ms())
+
+print("\n\nFINE MATERIA CLASS PROVA")
+
 #salva_su_file(file_name,lista_materie)
-lista_materie2 = leggi(file_name)
-print("Lettura Lista 2: ")
-for materia in lista_materie2:
-    print(f"{materia.get_n_materia()} -> {materia.get_time()}")
+#lista_materie2 = leggi(file_name)
+materia_holder.lista_materie = leggi(file_name)
+print("Lettura Lista 2: ")  
+for materia in materia_holder.lista_materie:
+    print(f"{materia.get_n_materia()} -> {materia.get_time()//60}h {materia.get_time()%60}m")
--- materia_class.py
+++ materia_class.py
@@ -9,6 +9,10 @@ class Materia:
     def get_time(self):
         return self.t_totale
     
+
+    def get_time_ms(self):
+        return f"{self.t_totale//60}h {self.t_totale%60}m"
+    
     def get_n_materia(self):
         return self.n_materia
     
